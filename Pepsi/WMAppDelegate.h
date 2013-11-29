d7826b126531131589a467a342489501eb1eef2b
Wraecca <wraecca@gmail.com>
2013-11-29 14:53:59 +0800

    add project & workspace

---
+++ Pepsi/WMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WMAppDelegate.h
+//  Pepsi
+//
+//  Created by Wraecca on 2013/11/29.
+//  Copyright (c) 2013年 WOOMOO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pepsi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pepsi
+//
+//  Created by Wraecca on 2013/11/29.
+//  Copyright (c) 2013年 WOOMOO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WMAppDelegate class]));
+    }
+}
